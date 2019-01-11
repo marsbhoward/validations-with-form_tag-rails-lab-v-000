66e5cb3439662500ca34604dcac9a4007c564be5
Mars Howard <marsbhoward@gmail.com>
2019-01-11 00:18:56 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
+  belongs_to :author
   validates :title, presence: true
   validates :content, length: { minimum: 100 }
-  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
