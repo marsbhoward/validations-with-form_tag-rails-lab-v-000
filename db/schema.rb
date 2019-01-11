e32b962f34721ed5200084825cd56a2366fdd44f
Mars Howard <marsbhoward@gmail.com>
2019-01-11 00:20:50 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151209022705) do
 
-  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
-    t.string   "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "authors", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
